a1cc393308b040391dd378b65f811bb2a37c5e8b
Saurabh Badhwar <sbsaurabhbadhwar9@gmail.com>
2018-04-13 16:14:39 +0530

    database: Refactor code to export the structure variables and add some code enhancements
    Signed-off-by: Saurabh Badhwar <sbsaurabhbadhwar9@gmail.com>

--- lib/database.go
+++ lib/database.go
@@ -7,11 +7,12 @@
 package inventory
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"os"
-	"encoding/json"
 	"sync"
+	"time"
 )
 
 const (
@@ -27,35 +28,35 @@ const (
 // individual hosts including their names and local variables.
 type Host struct {
 	// hostname The address through which the host can be reached
-	hostname string
+	Hostname string
 	// fcats The host specific variable
-	facts map[string]string
+	Facts map[string]string
 }
 
 // NewHost defines the initializer for creating a new host
 func NewHost(hostname string) *Host {
-	return &Host{hostname: hostname, facts: make(map[string]string)}
+	return &Host{Hostname: hostname, Facts: make(map[string]string)}
 }
 
 // GetHostName returns the hostname of the host
 func (h Host) GetHostName() string {
-	return h.hostname
+	return h.Hostname
 }
 
 // GetHostFacts returns the facts specific to the host
 func (h Host) GetHostFacts() map[string]string {
-	return h.facts
+	return h.Facts
 }
 
 // SetFact sets a new host fact as defined by the name and value
 func (h *Host) SetFact(name string, value string) {
-	h.facts[name] = value
+	h.Facts[name] = value
 }
 
 // DeleteFact deletes a host local fact from the mapping
 func (h *Host) DeleteFact(name string) {
-	if _, ok := h.facts[name]; ok {
-		delete(h.facts, name)
+	if _, ok := h.Facts[name]; ok {
+		delete(h.Facts, name)
 	}
 }
 
@@ -64,52 +65,48 @@ func (h *Host) DeleteFact(name string) {
 // service.
 type HostGroup struct {
 	// name defines the name of the hostgroup through which it can be identified
-	name string
+	Name string
 	// hosts defines a slice in which the hosts belonging to a particular hostgroup can be grouped together
-	hosts map[string]*Host
+	Hosts map[string]*Host
 }
 
 // NewHostGroup creates a new hostgroup for the inventory
 func NewHostGroup(name string) *HostGroup {
-	return &HostGroup{name: name, hosts: make(map[string]*Host)}
+	return &HostGroup{Name: name, Hosts: make(map[string]*Host)}
 }
 
 // AddHost adds a new host to the existing hostgroup
 func (hg *HostGroup) AddHost(h *Host) {
 	hostname := h.GetHostName()
-	if _, ok := hg.hosts[hostname]; !ok {
-		hg.hosts[hostname] = h
+	if _, ok := hg.Hosts[hostname]; !ok {
+		hg.Hosts[hostname] = h
 	}
 }
 
 // DeleteHost removes a host from the Hostgroup
-// TODO: Change the implementation to use hostname instead
-func (hg *HostGroup) DeleteHost(h *Host) {
-	for _, host := range hg.hosts {
-		if host == h {
-			host = nil
-		}
+func (hg *HostGroup) DeleteHost(hname string) {
+	if _, ok := hg.Hosts[hname]; ok {
+		hg.Hosts[hname] = nil
 	}
 }
 
 // GetHost returns the host object provided the host name
 // If the host doesn't exists, nil is returned
 func (hg HostGroup) GetHost(hname string) *Host {
-	if host, ok := hg.hosts[hname]; ok {
+	if host, ok := hg.Hosts[hname]; ok {
 		return host
 	}
 	return nil
 }
 
-
 // GetHosts returns all the hosts from a hostgroup
 func (hg HostGroup) GetHosts() map[string]*Host {
-	return hg.hosts
+	return hg.Hosts
 }
 
 // GetHostgroupName returns the name of the hostgroup
 func (hg HostGroup) GetHostgroupName() string {
-	return hg.name
+	return hg.Name
 }
 
 // Inventory struct defines the global service based inventory database
@@ -122,35 +119,49 @@ func (hg HostGroup) GetHostgroupName() string {
 type Inventory struct {
 	// hostgroups store the created hostgroups along with the data related
 	// to the individual hosts inside them.
-	hostgroups map[string]*HostGroup
+	Hostgroups map[string]*HostGroup
 	// dataStorePath defines the path where the inventory database is created
 	// on the disk. Whenever the service starts, it will look for the inventory
 	// database at the specified path and try to load the data from it.
-	dataStorePath string
+	DataStorePath string
 	// flushInterval defines the time in milliseconds at which the inventory
 	// flush service will write the data to the disk file.
-	flushInterval uint16
+	FlushInterval uint16
 	// pendingOps provide the information about how many operations are still
 	// pending to be written to the disk. This provides some data into how much
 	// data is inventory service storing in its volatile state. This parameter
 	// can also be used in future to enhance the inventory data flush service
 	// to be more consistent and aggressive in writing the inventory to disk.
-	pendingOps uint32
+	PendingOps uint32
 
 	// A Reader Writer mutex lock to help during the Marshalling of data
 	sync.RWMutex
+
+	// inventoryInactive defines a channel which is used to signal the
+	// goroutines that we are closing, and they need to exit
+	inventoryInactive chan bool
 }
 
 // NewInventory creates a new Inventory store to be used by the Inventory
 // Service.
-// TODO: Integrate the flush inventory service into the initializer
 func NewInventory(dataStorePath string, flushInterval uint16) *Inventory {
-	return &Inventory{
-		hostgroups:    make(map[string]*HostGroup),
-		dataStorePath: dataStorePath,
-		flushInterval: flushInterval,
-		pendingOps:    0,
+	if !checkDatastorePath(dataStorePath) {
+		ok, err := createDatastore(dataStorePath)
+		if ok != true {
+			log.Fatalf("Unable to create a datastore %s", err)
+		}
 	}
+
+	inv := Inventory{
+		Hostgroups:        make(map[string]*HostGroup),
+		DataStorePath:     dataStorePath,
+		FlushInterval:     flushInterval,
+		PendingOps:        0,
+		inventoryInactive: make(chan bool),
+	}
+
+	go inv.flushInventoryService()
+	return &inv
 }
 
 // toJSON converts the current state of the inventory structure to JSON
@@ -161,7 +172,7 @@ func (inv *Inventory) toJSON() []byte {
 	defer inv.RUnlock()
 	invJSON, err := json.Marshal(inv)
 	if err != nil {
-		log.Printf("Unable to encode the data as valid JSON", err)
+		log.Fatalf("Unable to encode the data as valid JSON", err)
 		return nil
 	}
 	return invJSON
@@ -171,9 +182,9 @@ func (inv *Inventory) toJSON() []byte {
 // data to the datastore.
 func (inv *Inventory) Save() {
 	// check if the datastore actually exists or not
-	if checkDatastorePath(inv.dataStorePath) == false {
+	if checkDatastorePath(inv.DataStorePath) == false {
 		// we don't have the data store present, try to create one
-		_, err := createDatastore(inv.dataStorePath)
+		_, err := createDatastore(inv.DataStorePath)
 		if err != nil {
 			log.Fatalf("Unable to create a datastore", err)
 		}
@@ -182,7 +193,7 @@ func (inv *Inventory) Save() {
 	if jsonData == nil {
 		log.Fatalf("Unable to convert the data into valid JSON")
 	}
-	if inv.WriteData(jsonData) == true {
+	if inv.WriteData(jsonData) == false {
 		log.Fatalf("Scheduled inventory save failed. Exiting...")
 	}
 }
@@ -193,7 +204,7 @@ func (inv *Inventory) Save() {
 func (inv *Inventory) WriteData(data []byte) bool {
 	inv.Lock()
 	defer inv.Unlock()
-	err := ioutil.WriteFile(inv.dataStorePath, data, 0644)
+	err := ioutil.WriteFile(inv.DataStorePath, data, 0644)
 	if err != nil {
 		log.Printf("File data write failed", err)
 		return false
@@ -205,16 +216,16 @@ func (inv *Inventory) WriteData(data []byte) bool {
 // inventory. If the hostgroup already exists, the call returns
 // without making any changes.
 func (inv *Inventory) NewHostgroup(hgname string) {
-	if _, ok := inv.hostgroups[hgname]; !ok {
+	if _, ok := inv.Hostgroups[hgname]; !ok {
 		hg := NewHostGroup(hgname)
-		inv.hostgroups[hgname] = hg
+		inv.Hostgroups[hgname] = hg
 	}
 }
 
 // GetHostgroup retrieves the hostgroup when the name is provided
 // if the hostgroup doesn't exists, the call returns a nil
 func (inv Inventory) GetHostgroup(hgname string) *HostGroup {
-	if hg, ok := inv.hostgroups[hgname]; ok {
+	if hg, ok := inv.Hostgroups[hgname]; ok {
 		return hg
 	}
 	return nil
@@ -253,7 +264,35 @@ func (inv *Inventory) SetHostFact(hgname string, hname string, fname string, fva
 		if host != nil {
 			host.SetFact(fname, fval)
 		}
-	} 
+	}
+}
+
+func (inv *Inventory) flushInventoryService() {
+	log.Printf("Starting the flushInventory service")
+	for {
+		select {
+		case sig := <-inv.inventoryInactive:
+			log.Printf("Shutdown signal received. Storing the structures and shutting down")
+			if sig == true {
+				inv.Save()
+				inv.inventoryInactive <- true
+				return
+			}
+		default:
+			inv.Save()
+			time.Sleep(time.Duration(inv.FlushInterval))
+		}
+	}
+}
+
+// StopInventory signals the inventory service to exit gracefully
+func (inv *Inventory) StopInventory() {
+	log.Printf("Shutdown request received. Signalling the routines to terminate")
+	inv.inventoryInactive <- true
+	sig := <-inv.inventoryInactive
+	if sig != true {
+		log.Fatalf("Error encountered while trying to shutdown the service")
+	}
 }
 
 // checkDatastorePath validates if a path provided exists on the disk or not
@@ -265,10 +304,10 @@ func checkDatastorePath(path string) bool {
 }
 
 // createDatastore creates a new datastore if it doesn't exists. In case of err
-// the bool parameter returns a false and an error type object which can be 
+// the bool parameter returns a false and an error type object which can be
 // queried for the error message
 func createDatastore(path string) (bool, error) {
-	f, err := os.OpenFile(path, os.O_RDONLY | os.O_CREATE, 0644)
+	f, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0644)
 	defer f.Close()
 	if err != nil {
 		return false, err
